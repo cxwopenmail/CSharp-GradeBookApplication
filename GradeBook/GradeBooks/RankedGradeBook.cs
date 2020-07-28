6838c6fdfe8a434275e9c07c71e748ce378046f4
Xinwei Cheng <xinwei@openmail.co>
2020-07-27 17:38:26 -0700

    create gradebook types

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,9 @@
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks {
+  public class RankedGradeBook: BaseGradeBook {
+    RankedGradeBook(string name): base(name) {
+      Type = GradeBookType.Ranked;
+    }
+  }
+}
